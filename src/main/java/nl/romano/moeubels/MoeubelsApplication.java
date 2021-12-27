44651972632d879f2cb4a246a082001a8ecaecf9
Romano999 <71527835+Romano999@users.noreply.github.com>
2021-12-27 16:44:27 +0100

    initial commit

---
+++ src/main/java/nl/romano/moeubels/MoeubelsApplication.java
@@ -0,0 +1,13 @@
+package nl.romano.moeubels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoeubelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoeubelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/romano/moeubels/MoeubelsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.romano.moeubels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoeubelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
