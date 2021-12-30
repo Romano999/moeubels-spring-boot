246e7172d75e44680d6ce75cd7b303b18011c9df
Romano999 <71527835+Romano999@users.noreply.github.com>
2021-12-30 13:16:07 +0100

    Added Project Lombok. Done to lower amount of boilerplate code and decrease overall verbosity.

--- src/main/java/nl/romano/moeubels/model/Actor.java
+++ src/main/java/nl/romano/moeubels/model/Actor.java
@@ -1,5 +1,9 @@
 package nl.romano.moeubels.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.json.JSONObject;
 
 import javax.persistence.*;
@@ -8,6 +12,10 @@ import java.util.UUID;
 
 @Entity
 @Table(name= "actor")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Actor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,77 +33,4 @@ public class Actor {
     private ZonedDateTime createdAt;
     @Column(name = "modified_at", nullable = false)
     private ZonedDateTime modifiedAt;
-
-    /**
-     * No args constructor for use in serialization
-     *
-     */
-    public Actor() {
-    }
-
-    public Actor(UUID actorId, String password, String username, String firstName, String lastName, ZonedDateTime createdAt, ZonedDateTime modifiedAt) {
-        this.actorId = actorId;
-        this.password = password;
-        this.username = username;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.createdAt = createdAt;
-        this.modifiedAt = modifiedAt;
-    }
-
-    public UUID getActorId() {
-        return actorId;
-    }
-
-    public void setActorId(UUID actorId) {
-        this.actorId = actorId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public ZonedDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(ZonedDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public ZonedDateTime getModifiedAt() {
-        return modifiedAt;
-    }
-
-    public void setModifiedAt(ZonedDateTime modifiedAt) {
-        this.modifiedAt = modifiedAt;
-    }
 }
