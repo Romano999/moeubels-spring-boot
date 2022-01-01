dc7b28c3ba921947732360d858af26c4256cde1d
Romano <romano1@live.nl>
2022-01-01 23:00:43 +0100

    Review endpoint functionality completed.

---
+++ src/main/java/nl/romano/moeubels/dao/ReviewDao.java
@@ -0,0 +1,38 @@
+package nl.romano.moeubels.dao;
+
+import nl.romano.moeubels.exceptions.ResourceNotFoundException;
+import nl.romano.moeubels.exceptions.ReviewNotFoundException;
+import nl.romano.moeubels.model.Review;
+import nl.romano.moeubels.repository.ReviewRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class ReviewDao implements Dao<Review> {
+    @Autowired
+    private ReviewRespository reviewRespository;
+
+    @Override
+    public Optional<Review> getById(UUID uuid) {
+        return reviewRespository.findById(uuid);
+    }
+
+    @Override
+    public void save(Review review) {
+        reviewRespository.save(review);
+    }
+
+    @Override
+    public void update(Review review) {
+        reviewRespository.save(review);
+    }
+
+    @Override
+    public void delete(UUID uuid) throws ResourceNotFoundException {
+        reviewRespository.findById(uuid)
+                .orElseThrow(() -> new ReviewNotFoundException("Review with id: " + uuid + "not found"));
+    }
+}
