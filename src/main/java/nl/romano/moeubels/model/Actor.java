c0318b792fe917ddaf23c16e15779a52538b239f
Romano <romano1@live.nl>
2022-01-01 13:20:01 +0100

    Json format of ZonedDateTime values in models changed.

--- src/main/java/nl/romano/moeubels/model/Actor.java
+++ src/main/java/nl/romano/moeubels/model/Actor.java
@@ -1,5 +1,6 @@
 package nl.romano.moeubels.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -39,8 +40,10 @@ public class Actor {
     private String lastName;
     @Column(name = "created_at", nullable = false)
     @JsonProperty("createdAt")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     private ZonedDateTime createdAt;
     @Column(name = "modified_at", nullable = false)
     @JsonProperty("modifiedAt")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     private ZonedDateTime modifiedAt;
 }
--- src/main/java/nl/romano/moeubels/model/Category.java
+++ src/main/java/nl/romano/moeubels/model/Category.java
@@ -1,5 +1,6 @@
 package nl.romano.moeubels.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
@@ -34,8 +35,10 @@ public class Category {
     private String categoryDescription;
     @Column(name = "created_at", nullable = false)
     @JsonProperty("createdAt")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     private ZonedDateTime createdAt;
     @Column(name = "modified_at", nullable = false)
     @JsonProperty("modifiedAt")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     private ZonedDateTime modifiedAt;
 }
