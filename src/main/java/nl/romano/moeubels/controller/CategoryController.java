6508d0d3ec501ee761d4bea2665d31bf03443835
Romano <romano1@live.nl>
2022-01-01 13:18:56 +0100

    Mapping to endpoints added.

--- src/main/java/nl/romano/moeubels/controller/CategoryController.java
+++ src/main/java/nl/romano/moeubels/controller/CategoryController.java
@@ -7,44 +7,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@RestController
 @RequestMapping("/categories")
 public class CategoryController implements CrudOperations<Category> {
     @Autowired
     private CategoryDao categoryDao;
 
-    @GetMapping("/all")
-    public ResponseEntity<?> getAll(int page) {
+    @GetMapping("/all/{page}")
+    public ResponseEntity<?> getAll(@PathVariable int page) {
         Page<Category> categories = categoryDao.getAll(Pageable.ofSize(5).withPage(page));
         return Responses.ResponseEntityOk(categories);
     }
 
     @Override
-    public ResponseEntity<?> getById(UUID uuid) throws Exception {
+    @GetMapping("/{uuid}")
+    public ResponseEntity<?> getById(@PathVariable UUID uuid) throws Exception {
         Category category = categoryDao.getById(uuid)
                 .orElseThrow(() -> new ResourceNotFoundException("Category with id: " + uuid + "not found"));
         return Responses.ResponseEntityOk(category);
     }
 
     @Override
+    @PostMapping()
     public ResponseEntity<String> create(@RequestBody Category category) {
         categoryDao.save(category);
         return Responses.jsonOkResponseEntity();
     }
 
     @Override
+    @PutMapping()
     public ResponseEntity<String> update(@RequestBody Category category) {
         categoryDao.save(category);
         return Responses.jsonOkResponseEntity();
     }
 
     @Override
-    public ResponseEntity<?> delete(UUID uuid) throws Exception {
+    @DeleteMapping("/{uuid}")
+    public ResponseEntity<?> delete(@PathVariable UUID uuid) throws Exception {
         categoryDao.delete(uuid);
         return Responses.jsonOkResponseEntity();
     }
